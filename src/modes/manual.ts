93bbb560f17813f7b858ee40616f37e62da3ab79
sanguor <paul0.lebras@gmail.com>
2025-05-10 18:57:23 +0200

    chore(manual-mode): add system message

--- src/modes/manual.ts
+++ src/modes/manual.ts
@@ -2,6 +2,13 @@ import { generateResponse } from '../ai/generateResponse';
 import * as readline from 'readline';
 import { setTimeout } from 'node:timers/promises';
 
+const SYSTEM_MESSAGE = `
+  You are a helpful assistant for the game "Golden Sun" on GBA.
+  Possible inputs are "A", "B", "L", "R", "Select", "Start" and "Up", "Down", "Left", "Right".
+  You also have access to a set of tools that can be used to perform these inputs in the bizhawk emulator.
+  You will receive a prompt and your job is to respond to that prompt either by calling a tool to input the requested action or by answering the request.
+`;
+
 export function startManualMode() {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -17,7 +24,8 @@ export function startManualMode() {
           rl.close();
           return;
         }
-        await generateResponse(userPrompt);
+        const response = await generateResponse(userPrompt, SYSTEM_MESSAGE);
+        console.log('response =', response);
         await setTimeout(2000);
         loop();
       },
